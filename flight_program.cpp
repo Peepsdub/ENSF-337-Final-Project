102a49d8f27a4bcffbfb72f7fb6bd919ff2b6239
Peepsdub <71529633+Peepsdub@users.noreply.github.com>
2023-11-23 16:17:48 -0700

    Update flight_program.cpp

--- flight_program.cpp
+++ flight_program.cpp
@@ -3,3 +3,15 @@
 //Version: 1.0
 //Term Project - Flight Management Program in C++
 //Produced by:
+
+
+// member functions for all: setters, getters, ctor, copy ctor, dector
+
+/*
+class flight
+flight id: string
+rows: int
+cols: int
+list of passengers: vector
+seat map: vector or matrix
+*/
