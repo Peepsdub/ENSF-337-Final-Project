51cecefbfa2d5d91dd86d1678f0ff19c8ed6e617
JakubS2 <144714970+JakubS2@users.noreply.github.com>
2023-11-28 16:24:07 -0700

    Update flight_program.cpp

--- flight_program.cpp
+++ flight_program.cpp
@@ -1,8 +1,6 @@
-
-
-//Version: 1.0
-//Term Project - Flight Management Program in C++
-//Produced by:
+Version: 1.0
+Term Project - Flight Management Program in C++
+Produced by: Dubovsky Petr, Stefanczyk Jakub, Lalu Neha 
 
 
 // member functions for all: setters, getters, ctor, copy ctor, dector
