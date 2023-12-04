6d6e067e37ab79c7913e1bff76f0afd724a86ecc
JakubS2 <144714970+JakubS2@users.noreply.github.com>
2023-12-03 20:53:16 -0700

    Update airline.h

--- airline.h
+++ airline.h
@@ -1,5 +1,5 @@
 //airline class header
-#ifndef AIRLINE_H
+/*#ifndef AIRLINE_H
 #define AIRLINE_H
 #include <stdio.h>
 #include <vector>
@@ -17,4 +17,22 @@ private:
     int num_flights;
 
 };
+#endif
+*/
+#ifndef AIRLINE_H
+#define AIRLINE_H
+
+#include "Flight.h"
+
+class Airline {
+public:
+    Airline(const std::string& airlineName);  // Constructor
+    void showHeader() const;  // Renamed: display_header() to showHeader()
+    void loadFlightFromFile(Flight& flight, const std::string& filename);  // Renamed: populate_flight_from_file() to loadFlightFromFile()
+    void displayMenu(Flight& flight, const std::string& filename);  // Renamed: menu() to displayMenu()
+
+private:
+    std::string airlineName;  // Added: member variable to store the airline's name
+};
+
 #endif
