bc5d6fc41e9319e5a430ba2fa893b7ecd8e0ddd2
JakubS2 <144714970+JakubS2@users.noreply.github.com>
2023-12-03 21:28:27 -0700

    Update airline.cpp

--- airline.cpp
+++ airline.cpp
@@ -17,7 +17,7 @@ void Airline::loadFlightFromFile(Flight& flight, const std::string& filename) {
     }
 }
 
-void Airline::displaydisplayMenu(Flight& flight, const std::string& filename) {
+void Airline::displayMenu(Flight& flight, const std::string& filename) {
     int q;
     do {
         std::cout << "\nMenu Options:\n1. Show map of seats\n2. Passenger Info\n3. Add Passenger\n4. Remove Passenger\n5. Save Passenger Info\n6. Exit" << std::endl;
