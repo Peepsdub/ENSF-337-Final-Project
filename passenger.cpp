e1f335f8975d81aaa2baf47e908fac61236ed0c6
Peepsdub <71529633+Peepsdub@users.noreply.github.com>
2023-12-03 21:19:34 -0700

    Update passenger.cpp

--- passenger.cpp
+++ passenger.cpp
@@ -1,4 +1,4 @@
-#include "Passenger.h"
+#include "passenger.h"
 Passenger::Passenger(const std::string& first, const std::string& last, const std::string& phone, Seat* seat, int id)
     : FName(first), LName(last), PNumber(phone), assignedSeat(seat), idNumber(id) {}
 std::string Passenger::getFirstName() const {
