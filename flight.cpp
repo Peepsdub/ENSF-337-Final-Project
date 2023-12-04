f356edb6d927d09aa0d51fd5cb2688bfc36358f1
Peepsdub <71529633+Peepsdub@users.noreply.github.com>
2023-12-03 21:34:13 -0700

    Update flight.cpp

--- flight.cpp
+++ flight.cpp
@@ -9,31 +9,6 @@ seat map: vector or matrix
 */
 
 #include "flight.h"
-#include "seat.cpp"
-Flight::Flight(int r, int c):rows(r), cols(c) {
-    vector <vector <Seat*>> seat_map;
-    seat_map.resize(rows);
-
-    for (int i = 0; i < rows; i++)
-        seat_map.at(i).resize(cols);
-    
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            (*seat_map.at(i).at(j)).set_row(i);
-            (*seat_map.at(i).at(j)).set_col(num_to_alpha(j));
-        }
-    }
-}
-
-
-char num_to_alpha(int i) {
-    if (i<26)
-        i += 97;
-    else
-        i += 65;
-    return char(i);
-}
-#include "flight.h"
 #include <iostream>
 #include <iomanip>
 #include <fstream>
