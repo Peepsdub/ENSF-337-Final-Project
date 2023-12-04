33c912dc10a0fed51f1f7652975d796ad9d922ff
JakubS2 <144714970+JakubS2@users.noreply.github.com>
2023-12-03 21:13:02 -0700

    Update flight_program.cpp

--- flight_program.cpp
+++ flight_program.cpp
@@ -1,27 +1,10 @@
-//Version: 1.0
-//Term Project - Flight Management Program in C++
-//Produced by: Dubovsky Petr, Stefanczyk Jakub, Lalu Neha 
-#include <iostream>
-using namespace std;
-
-void menu();
-
-int main () {
-    cout << "Version: 1.0" << endl;
-    cout << "Term Project - Flight Management Program in C++" << endl;
-    cout << "Produced by: Dubovsky Petr, Stefanczyk Jakub, Lalu Neha" << endl;
-    cout << "<<< Press Return to Continue >>>" << endl;
+#include "Airline.h"
+int main() {
+    Airline A;
+    Flight flight;
+    std::string filename = "flight_info.txt";
+    A.display_header();
+    A.populate_flight_from_file(flight, filename);
+    A.menu(flight, filename);
+    return 0;
 }
-
-void menu() {
-    cout << "Please select on of the following options" << endl;
-    cout << "1. Display flight seat map"  << endl;
-    cout << "2. Display passengers information" << endl;
-    cout << "3. Add a new passenger" << endl;
-    cout << "4. Remove an existing passenger" << endl;
-    cout << "5. Save data" << endl;
-    cout << "6. Quit" << endl;
-
-}
-
-// member functions for all: setters, getters, ctor, copy ctor, dector
