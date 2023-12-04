eaa7132bf4adccbfde5d8c1bb0188bce107e70c2
JakubS2 <144714970+JakubS2@users.noreply.github.com>
2023-12-03 21:03:23 -0700

    Update seat.h

--- seat.h
+++ seat.h
@@ -1,47 +1,13 @@
-//seat class header
-/*#ifndef SEAT_H
-#define SEAT_H
-#include <stdio.h>
-using namespace std;
-class Seat {
-public:
-    Seat():row(0), col(' '), status(false){}
-    ~Seat();
-
-    void set_row(int r) {row = r;}
-    void set_col(int c) {col = c;}
-    void set_status(bool s) {status = s;}
-    bool get_status() const {return status;}
-
-private:
-    int row;
-    char col;
-    bool status;
-};
-
-
-#endif
-*/
-
 
 #ifndef SEAT_H
 #define SEAT_H
-
 class Seat {
-
 public:
-
     Seat (int r, char s);
-
     int getRow() const { return row; }
-
     char getSeat() const { return seat; }
-
 private:
-
     int row;
-
     char seat;
 };
-
 #endif
