8ee2fd39d4e723f3c47b616ca9cc5c9d053d3129
JakubS2 <144714970+JakubS2@users.noreply.github.com>
2023-12-03 21:41:56 -0700

    Update flight_program.cpp

--- flight_program.cpp
+++ flight_program.cpp
@@ -3,8 +3,8 @@ int main() {
     Airline airline;
     Flight flight;
     std::string filename = "flight_info.txt";
-    airline.showheader();
-    airline.populate_flight_from_file(flight, filename);
-    airline.menu(flight, filename);
+    airline.showHeader();
+    airline.loadFlightFromFile(flight, filename);
+    airline.displayMenu(flight, filename);
     return 0;
 }
