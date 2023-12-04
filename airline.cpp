8a6f859793b5bdfe9332e0079090550283ad4f69
JakubS2 <144714970+JakubS2@users.noreply.github.com>
2023-12-03 20:24:27 -0700

    Update airline.cpp

--- airline.cpp
+++ airline.cpp
@@ -23,41 +23,42 @@ void Airline::populate_flight_from_file(Flight& flight, const std::string& filen
 }
 
 void Airline::menu(Flight& flight, const std::string& filename) {
-    int op;
+    int q;
     do {
         std::cout << "\nMenu Options:\n1. Show map of seats\n2. Show Passenger Info\n3. Add Passenger\n4. Remove Passenger\n5. Save Passenger Info\n6. Exit" << std::endl;
         std::cout << "Enter an option: ";
-        std::cin >> op;
+        std::cin >> q;
         std::cin.clear();
         std::cin.ignore(10000, '\n');
 
-        if (op == 1) {
+        if (q == 1) {
             flight.show_seat_map();
         }
-        else if (op == 2) {
+        else if (q == 2) {
             flight.show_passenger_info();
         }
-        else if (op == 3) {
+        else if (q == 3) {
             flight.add_passenger();
         }
-        else if (op == 4) {
+        else if (q == 4) {
             flight.remove_passenger();
         }
-        else if (op == 5) {
+        else if (q == 5) {
             flight.save_passenger_info(filename);
         }
-        else if (op == 6) {
+        else if (q == 6) {
             std::cout << "Exiting program." << std::endl;
         }
         else {
             std::cout << "Not a choice. Please try again." << std::endl;
         }
 
-        if (op != 6) {
+        if (q != 6) {
             std::cout << "<<<Press Return to Continue...>>>";
             std::cin.get(); 
         }
-    } while (op != 6);
+    } while ( q!= 6);
 }
+
 
 //Airline::Airline (string n): name(n) {}
